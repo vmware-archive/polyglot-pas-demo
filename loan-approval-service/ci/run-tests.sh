d1d48b9dc593e75bf0406ee2abf86532bd3eaacc
benwilcock <benwilcock@gmail.com>
2019-04-30 12:15:18 +0100

    added more detail to readme and helped the app run locally

--- loan-approval-service/ci/run-tests.sh
+++ loan-approval-service/ci/run-tests.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
+set -eux
 
-if [ -n $checkerURL ]
+if [ -n ${checkerURL} ]
 then
     export checkerURL=http://loan-checker-test.apps.cloudyazure.io
     echo -e "Setting URL for the Loan Checker to ${checkerURL}"
 fi
 
-env URL="$checkerURL" ./tasks/smoke-test-loan-checker.sh
+env URL="${checkerURL}" ./tasks/smoke-test-loan-checker.sh
